cd19fe2d1bac7eab0186700ceb3e2ee5797302b2
khefifi-Adem <ademkhefifi123@gmail.com>
2022-09-21 12:33:48 +0100

    delete the h1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import Layout from '../components/Layout';
 
 export default function Home() {
-  return (
-    <>
-      <Layout title="Home Page">Home page</Layout>
-      <h1 className="text-3xl font-bold">Hello world</h1>
-    </>
-  );
+  return <Layout title="Home Page">Home page</Layout>;
 }
